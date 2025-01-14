78c001f7a641aad230a469699561485eba00b88f
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-01-14 16:46:29 -0300

    função verifica tamanho window

--- js/modules/contentCard.js
+++ js/modules/contentCard.js
@@ -1,3 +1,5 @@
+import initChangeImage from "./changeImage.js";
+
 export default function initContentCard() {
     const cards = document.querySelectorAll("[data-card]");
     if (cards.length) {
@@ -15,27 +17,15 @@ export default function initContentCard() {
         function inserirDados(dadosJson) {
             cards.forEach((card, index) => {
                 const img = card.querySelector(".container-imagem img");
-                const paragrafoCard = card.querySelector(
-                    ".container-descricao p"
-                );
-                const tituloCard = card.querySelector(
-                    ".container-descricao h2"
-                );
-                const precoCard = card.querySelector(
-                    ".container-descricao .preco"
-                );
-                const {
-                    image: { desktop },
-                    category,
-                    name,
-                    price,
-                } = dadosJson[index];
+                const paragrafoCard = card.querySelector(".container-descricao p");
+                const tituloCard = card.querySelector(".container-descricao h2");
+                const precoCard = card.querySelector(".container-descricao .preco");
+                const { image, category, name, price } = dadosJson[index];
 
+                initChangeImage(img, image, name);
                 tituloCard.innerText = name;
                 precoCard.innerText += price;
                 paragrafoCard.innerText = category;
-                img.setAttribute("src", desktop);
-                img.setAttribute("alt", name);
             });
         }
     }
