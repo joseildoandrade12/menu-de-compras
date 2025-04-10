c8074e8ff115dc3f3acbdc52160daba50f56c8f0
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-04-10 00:29:07 -0300

    adição do evento de remover item do carrinho

---
+++ js/modules/closedItemCart.js
@@ -0,0 +1,22 @@
+import changeQuantityItem from "./changeQuantityItem.js";
+import changeValueTotal from "./changeValueTotal.js";
+import removeClasses from "./removeClasses.js";
+
+export default function closedItemCart() {
+    const buttonsClosed = document.querySelectorAll(".icon-fechar");
+    const containerGrid = document.querySelector(".container-grid");
+    buttonsClosed.forEach((button) => {
+        button.addEventListener("click", excluirElemento);
+    });
+
+    function excluirElemento(event) {
+        const itemCarrinhoId = event.currentTarget.parentElement.getAttribute("id");
+        const section = containerGrid.querySelector(`#\\3${itemCarrinhoId} `);
+        const buttonAdd = section.querySelector(".btn-adicionar");
+        const quantidadeItens = buttonAdd.querySelector(".contador-quantidade");
+        quantidadeItens.innerText = 1;
+        removeClasses(buttonAdd);
+        changeQuantityItem(itemCarrinhoId, 0);
+        changeValueTotal();
+    }
+}
